b74490f2e6d272cd00f33e86bfecf9282b1e47df
niran manandhar <nirnaism@gmail.com>
2016-09-05 20:35:57 -0500

    added color variable

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+function randomColor() {
+  var colors = ["#FFBF00", "#0080FF", "#01DF3A", "#FF0080"];
+  // return colors[]
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Emergency Comlipent' });
+});
+
+
+
+
+module.exports = router;
