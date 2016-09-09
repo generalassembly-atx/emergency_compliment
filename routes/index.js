07d38761cdc64531903ea300b90b84934c65d021
jesse houghton <OiseauNoir>
2016-09-09 15:00:39 -0500

    refactored compliment - now working

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Amanda thinks you're wicked smart!",
+  "Britney Jo is sooo proud of you! :)",
+  "Sean would totally hire you.",
+  "It's almost beer o'clock!"
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let compliment = compliments[Math.floor(Math.random()*compliments.length)]
   res.render('index', {
     title: 'Emergency Compliment!',
-    compliment: getCompliment()
+    compliment: compliment
   });
 });
 
-function getCompliment() {
-  var compliments = [
-    "Your instructors love you",
-    "High five = ^5",
-    "Amanda thinks you're wicked smart!",
-    "Britney Jo is sooo proud of you! :)",
-    "Sean would totally hire you.",
-    "It's almost beer o'clock!"
-  ]
-  return compliments[Math.floor(Math.random()*compliments.length)]
-}
+
+
+
 
 function randomColor() {
   var colors = ["#fd6c3b", "#4edacf","#65a576","#f2d83d"];
