7bccb4154ff7253b2a16addc6b252c72e23fbd45
jesse houghton <OiseauNoir>
2016-09-08 21:31:07 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Emergency Compliment!' });
+});
+
+
+var compliments = [
+  "Your instructors love you",
+  "High five = ^5",
+  "Amanda thinks you're wicked smart!",
+  "Britney Jo is sooo proud of you! :)",
+  "Sean would totally hire you.",
+  "It's almost beer o'clock!"
+]
+
+var colors = ["#fd6c3b", "#4edacf","#65a576","#f2d83d"];
+
+module.exports = router;
