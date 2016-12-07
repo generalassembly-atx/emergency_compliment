204e6eaef78a00259208f5cedd3f8096b6f2e471
Carter Kessler <rckessler84@yahoo.com>
2016-12-07 08:51:37 -0600

    Couldn't start express

---
+++ carter/routes/index.js
@@ -0,0 +1,20 @@
+// var express = require('express');
+// var router = express.Router();
+//
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+//
+// module.exports = router;
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
